dc1e376b6af6b1397129d9ff36ac8495bda1799f
BACONROLLS <140130015+baconrolls@users.noreply.github.com>
2023-07-30 02:02:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -50,60 +50,74 @@ const messages = [
     // Add more messages for Leo as needed
   ],
 
-  // Messages for Card 6
+  // Messages for Virgo
   [
-    "Message 1 for Card 2.",
-    "Message 2 for Card 2.",
-    "Message 3 for Card 2.",
-    // Add more messages for Card 2 as needed
+"Your attention to detail and analytical mind make you an excellent problem solver.",
+"Embrace your desire for order and organization; it brings a sense of calm.",
+"Your thoughtful nature makes you a reliable and supportive friend.",
+"Trust your practical instincts when making decisions; they serve you well.",
+"Focus on your well-being and practice self-care; it rejuvenates your spirit.",
+    // Add more messages for Virgo as needed
   ],
 
-    // Messages for Card 7
+    // Messages for Libra
   [
-    "Message 1 for Card 1.",
-    "Message 2 for Card 1.",
-    "Message 3 for Card 1.",
-    // Add more messages for Card 1 as needed
+"Your diplomatic skills make you an excellent mediator and peacemaker.",
+"Embrace your sense of harmony and seek balance in all aspects of life.",
+"Your charm and grace create a positive and welcoming atmosphere.",
+"Cultivate meaningful connections by being attentive to others' needs.",
+"Trust your intuition when it comes to making fair and just decisions.",
+    // Add more messages for Libra as needed
   ],
 
-  // Messages for Card 82
+  // Messages for Scorpio
   [
-    "Message 1 for Card 2.",
-    "Message 2 for Card 2.",
-    "Message 3 for Card 2.",
-    // Add more messages for Card 2 as needed
+"Your intense passion and determination drive you towards success.",
+"Embrace your emotional depth and use it as a source of strength.",
+"Your intuitive nature allows you to read situations and people accurately.",
+"Cultivate trust in your relationships; your loyalty is unwavering.",
+"Embrace transformation; it leads to profound personal growth.",
+    // Add more messages for Scorpio as needed
   ],
 
-    // Messages for Card 9
+    // Messages for Sagittarius
   [
-    "Message 1 for Card 1.",
-    "Message 2 for Card 1.",
-    "Message 3 for Card 1.",
-    // Add more messages for Card 1 as needed
+"Your adventurous spirit leads you to exciting journeys and experiences.",
+"Embrace your optimistic outlook; it attracts positive opportunities.",
+"Your love for learning drives you to expand your knowledge and horizons.",
+"Embrace your candid nature; it makes you a refreshing and honest friend.",
+"Trust your intuition when seeking new adventures and opportunities.",
+    // Add more messages for Sagittarius as needed
   ],
 
-  // Messages for Card 10
+  // Messages for Capricorn
   [
-    "Message 1 for Card 2.",
-    "Message 2 for Card 2.",
-    "Message 3 for Card 2.",
-    // Add more messages for Card 2 as needed
+"Your determination and hard work lead you to achieve your ambitions.",
+"Embrace your responsible nature; you can be relied upon by others.",
+"Your patience and discipline bring success in your endeavors.",
+"Cultivate strong foundations in your relationships; they stand the test of time.",
+"Trust your ability to plan and strategize; it leads to steady progress.",
+    // Add more messages for Capricorn as needed
   ],
 
-    // Messages for Card 11
+    // Messages for Aquarius
   [
-    "Message 1 for Card 1.",
-    "Message 2 for Card 1.",
-    "Message 3 for Card 1.",
-    // Add more messages for Card 1 as needed
+"Your innovative thinking leads you to unique and groundbreaking ideas.",
+"Embrace your humanitarian spirit; make a positive impact on the world.",
+"Your open-mindedness allows you to embrace diversity and new perspectives.",
+"Cultivate meaningful friendships that share your values and passions.",
+"Trust your intuition when advocating for social change and progress.",
+    // Add more messages for Aquarius as needed
   ],
 
-  // Messages for Card 12
+  // Messages for Pisces
   [
-    "Message 1 for Card 2.",
-    "Message 2 for Card 2.",
-    "Message 3 for Card 2.",
-    // Add more messages for Card 2 as needed
+"Your compassionate nature makes you a source of comfort for others.",
+"Embrace your artistic talents and express yourself through creativity.",
+"Your intuition guides you in making heartfelt and empathetic decisions.",
+"Cultivate a sense of inner peace by spending time in introspection.",
+"Trust your dreams and imagination; they hold the key to your aspirations.",
+    // Add more messages for Pisces as needed
   ],
     
 ];
